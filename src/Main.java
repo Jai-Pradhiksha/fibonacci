2e3614cef9d85542ef47d48589a538697339281f
jaipradhiksha <pradhikshajai@gmail.com>
2023-08-10 12:23:31 +0530

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,50 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+/*
+
+//factorial
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int inputNumber=5;
+        FactorialFinder factorialFinder=new FactorialFinder();
+        System.out.println(factorialFinder.factorial(inputNumber));
+    }
+}
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+class FactorialFinder{
+    int factorial(int number)
+    {
+        if (number==1)
+        {
+            return 1;
         }
+        return number*factorial(number-1);
+    }
+}*/
+
+//fibonacci to print nth number
+public class Main {
+    public static void main(String[] args) {
+        int inputNumber = 3;
+        FibonacciSeries fibonacciseries = new FibonacciSeries();
+        System.out.println(fibonacciseries.fibonacci(inputNumber));
+        //greet();
+    }
+        /*
+        //static example
+        static void greet()
+        {
+            System.out.println("Hello");
+        }*/
+
+}
+
+class FibonacciSeries {
+    int fibonacci(int number) {
+        if (number<=1){
+            return number;
+        }
+        return fibonacci(number-1)+ fibonacci(number-2);
+
     }
 }
